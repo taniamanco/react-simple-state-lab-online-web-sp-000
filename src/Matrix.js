f1a309e61dcfeda92abf2c202d0bdbefe09e01eb
Tania Manco <belalov.tania16@gmail.com>
2020-01-09 20:11:20 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -19,7 +19,7 @@ export default class Matrix extends Component {
     )
   }
 
-  defaultProps = {
+  Matrix.defaultProps = {
     values: (() => {
       const defineRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
       return (new Array(10).fill(defineRow))
